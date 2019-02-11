b836efbcace67af2caf005ea4f0bb21ef1e47c78
Danny Vail <danny_vail@outlook.com>
2019-02-11 18:16:33 -0500

    WRITE /NAMES/BEGIN?LETTER= ENDPOINT In CountryController.java - Add an endpoint to return the countries alphabetically that begin with the given suffix

--- src/main/java/com/lambdaschool/countries/controllers/CountryController.java
+++ src/main/java/com/lambdaschool/countries/controllers/CountryController.java
@@ -2,7 +2,9 @@ package com.lambdaschool.countries.controllers;
 
 import com.lambdaschool.countries.CountriesApplication;
 import com.lambdaschool.countries.models.Country;
+import com.lambdaschool.countries.models.CountryList;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,5 +15,13 @@ public class CountryController {
   public ArrayList<Country> getCountries() {
     CountriesApplication.countries.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
     return CountriesApplication.countries.countryList;
+  }
+
+  @GetMapping("/names/begin")
+  public ArrayList<Country> getCountriesBegin(@RequestParam(value="letter") String letter) {
+    CountryList result =
+            new CountryList(CountriesApplication.countries.search(c -> c.getName().startsWith(letter)));
+    result.countryList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
+    return result.countryList;
   }
 }
